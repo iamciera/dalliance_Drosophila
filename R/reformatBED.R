37e1f39d33c2ef2c9a823dc3e7b49e9dc8994b1d
Ciera Martinez <ciera.martinez@gmail.com>
2016-03-22 15:33:23 -0700

    changed R bed handeling

--- R/reformatBED.R
+++ R/reformatBED.R
@@ -1,27 +1,29 @@
 ## Organizing .bed file
 ## Need BED12 format
 
-dm6 <- read.delim("dm6_refGenes.bed",header=F,as.is=T)
+dm6 <- read.delim("../data/dm6_refGenes.bed",header=F,as.is=T)
 
 head(dm6)
 summary(dm6)
 
+dm6[1,] # look at the names of the columns
+
 #remove column names
-new_dm6 <- dm6[-1,]
+new_dm6a <- dm6[-1,]
 
 # Rearrange Columns
 
-new_dm6 <- new_dm6[,c(3,7,8,9,4,12,5,6)]
+new_dm6b <- new_dm6a[,c(3,7,8,2,12,14,5,6)]
 
-head(new_dm6)
+head(new_dm6b)
 
 # Add RGB column
-new_dm6$RGB <- "255,0,0"
+new_dm6b$RGB <- "255,0,0"
 
-new_dm6 <- cbind(new_dm6, dm6[,c(9,10,11)])
+new_dm6c <- cbind(new_dm6b, new_dm6a[,c(16,10,11)])
 
-head(new_dm6)
+head(new_dm6c)
 
 # write out
 
-write.table(new_dm6,"dm6_BED12.bed",col.names=F,row.names=F,sep="\t",quote=F)
+write.table(new_dm6c,"dm6_BED12.bed",col.names=F,row.names=F,sep="\t",quote=F)
